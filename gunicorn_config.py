224764015adf77cf32b1f1e66857b23b1214b142
armin-bc <155591762+armin-bc@users.noreply.github.com>
2025-05-23 15:15:31 +0200

    Add files via upload

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,4 +2,4 @@ bind = "0.0.0.0:10000"
 workers = 2
 forwarded_allow_ips = "*"
 secure_scheme_headers = {"X-Forwarded-Proto": "https"}
-timeout = 120  # Longer timeout for file uploads
+timeout = 600  # Longer timeout for file uploads
